f129dcc84f77b84d58f7d1a1c0ad816e2cd55f6a
Ian <Iannakashima8@gmail.com>
2025-03-16 10:23:05 +0000

    TeST

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -55,15 +55,15 @@ void load_words(set<string> & word_list, const string& file_name){
     }
 }
 void print_word_ladder(const vector<string>& ladder){
-    if(!ladder.empty()){
+    if(ladder.empty()){
+        cout << "No word ladder found." << endl;
+        cout << "";
+    }else{
         cout << "Word ladder found: ";
         for(size_t i = 0; i < ladder.size(); ++i){
             cout << ladder[i] << " ";
         }
         cout << "" << endl;
-    }else{
-        cout << "No word ladder found." << endl;
-        cout << "";
     }
 }
 // void verify_word_ladder(){
